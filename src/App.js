2fdce2eae5958faed8a5fcaec2936cc397a14865
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-03-28 20:17:14 -0300

    Updated

--- src/App.js
+++ src/App.js
@@ -21,29 +21,8 @@ function App() {
   };
 
   const dateBuilder = (d) => {
-    let months = [
-      "01",
-      "02",
-      "03",
-      "04",
-      "05",
-      "06",
-      "07",
-      "08",
-      "09",
-      "10",
-      "11",
-      "12"
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
+    let months = [ "01","02", "03", "04","05","06","07","08", "09", "10","11","12"];
+    let days = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
     let day = days[d.getDay()];
     let date = d.getDate();
