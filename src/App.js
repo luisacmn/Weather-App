7229362e9b79a3ec19c72319fe5770a1059173dd
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-03-30 16:12:15 -0300

    Upload -Add search icon -Add description -Add weather icon -Add comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
+import { BiSearch } from "react-icons/bi";
+
 const api = {
   key: "8f65b8fc54f51dbf0663631455270d6c",
   base: "https://api.openweathermap.org/data/2.5/"
 };
 
 function App() {
-  const [query, setQuery] = useState(""); //query is th city name
+  const [query, setQuery] = useState(""); //query is th city name on the input. Its empty
   const [weather, setWeather] = useState({});
+
+  const btnclick = () => {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`) //fetch(url) method to request data from this URL API
+      .then((res) => res.json()) //Promise: response as a json file
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+        console.log(result);
+      });
+  };
 
   const search = (evt) => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`) //fetch(url) method to request data from this URL
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`) //fetch(url) method to request data from this URL API
         .then((res) => res.json()) //Promise: response as a json file
         .then((result) => {
           setWeather(result);
@@ -20,9 +32,33 @@ function App() {
     }
   };
 
-  const dateBuilder = (d) => {
-    let months = [ "01","02", "03", "04","05","06","07","08", "09", "10","11","12"];
-    let days = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  const dateBuilder = () => {
+    //React não aceita função como filha. Se o html direciona para uma função, tem que usar ela como uma arrow function e guardar em uma const
+    const d = new Date();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    let months = [
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12"
+    ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
@@ -44,8 +80,12 @@ function App() {
             value={query}
             onKeyPress={search}
           />
+          <span className="searchIcon" onClick={btnclick}>
+            {" "}
+            <BiSearch />{" "}
+          </span>
         </div>
-        {typeof weather.main !== "undefined" ? ( //conditional
+        {typeof weather.main !== "undefined" ? ( //conditional: Se o main de weather não for undefenined, mostrar a estrutura abaixo:
           <div>
             <div className="location-box">
               <div className="location">
@@ -55,7 +95,16 @@ function App() {
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div>
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="Weather Icon"
+                />{" "}
+              </div>
               <div className="weather">{weather.weather[0].main}</div>
+              <div className="description">
+                {weather.weather[0].description}
+              </div>
             </div>
           </div>
         ) : (
@@ -67,3 +116,7 @@ function App() {
 }
 
 export default App;
+
+//O método getMonth() retorna o mês na data especificada de acordo com o horário local, como um valor zero-based (onde o zero indica o primeiro mês do ano).
+//O método getDay() retorna o dia da semana para a data especificada de acordo com a hora local, onde 0 representa o Domingo.
+//Usei o site do React-icons para o ícone de pesquisa (nome do ícone de acordo com o fornecedor)
